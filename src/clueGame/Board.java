c591e61554cb217e7c2fa62cf3ff179205ed36cb
Alexander Schoep <aschoep@adit.mines.edu>
2016-10-04 13:26:02 -0600

    edited class skeleton for Board

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -2,10 +2,23 @@ import java.util.*;
 package clueGame;
 
 public class Board {
+	/*
 	private int numRows;
 	private int numColumns;
 	private static final int MAXBOARDSIZE = 50;
 	private BoardCell board[][];
 	private Map rooms;
 	private Map adjMatrix;
+	*/
+	
+	// variable used for singleton pattern
+	private static Board theInstance = new Board();
+	
+	// ctor is private to ensure only one can be created
+	private Board() {}
+	
+	// this method returns the only Board
+	public static Board getInstance() {
+		return theInstance;
+	}
 }
