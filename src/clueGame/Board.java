6c70281b1aed3450e5f486ffbd443e0241e41bb9
Alexander Schoep <aschoep@adit.mines.edu>
2016-10-04 13:12:37 -0600

    added a few things for ClueBoard 1

---
+++ src/clueGame/Board.java
@@ -0,0 +1,11 @@
+import java.util.*;
+package clueGame;
+
+public class Board {
+	private int numRows;
+	private int numColumns;
+	private static final int MAXBOARDSIZE = 50;
+	private BoardCell board[][];
+	private Map rooms;
+	private Map adjMatrix;
+}
