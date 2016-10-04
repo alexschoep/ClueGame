d0b95e79d3c64285de3a778ba5212b9a7a429b33
will brown <will brown>
2016-10-04 14:32:00 -0600

    Finalized JUnit tests and text descriptions.

---
+++ src/tests/OurTests.java
@@ -0,0 +1,119 @@
+package tests;
+
+/*
+ * This program tests that config files are loaded properly.
+ */
+
+// Doing a static import allows me to write assertEquals rather than
+// Assert.assertEquals
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+import clueGame.DoorDirection;
+
+public class OurTests {
+	// Constants that I will use to test whether the file was loaded correctly
+	public static final int LEGEND_SIZE = 11;
+	public static final int NUM_ROWS = 20;
+	public static final int NUM_COLUMNS = 20;
+
+	// NOTE: I made Board static because I only want to set it up one 
+	// time (using @BeforeClass), no need to do setup before each test.
+	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("map.csv", "legend.txt");		
+		// Initialize will load BOTH config files 
+		board.initialize();
+	}
+	@Test
+	public void testRooms() {
+		// Get the map of initial => room 
+		Map<Character, String> legend = board.getLegend();
+		// Ensure we read the correct number of rooms
+		assertEquals(LEGEND_SIZE, legend.size());
+		// To ensure data is correctly loaded, test retrieving a few rooms 
+		// from the hash, including the first and last in the file and a few others
+		assertEquals("Armory", legend.get('A'));
+		assertEquals("Rec Room", legend.get('B'));
+		assertEquals("Pool Room", legend.get('G'));
+		assertEquals("Dining Room", legend.get('H'));
+		assertEquals("Walkway", legend.get('W'));
+	}
+	
+	@Test
+	public void testBoardDimensions() {
+		// Ensure we have the proper number of rows and columns
+		assertEquals(NUM_ROWS, board.getNumRows());
+		assertEquals(NUM_COLUMNS, board.getNumColumns());		
+	}
+	
+	// Test a doorway in each direction (RIGHT/LEFT/UP/DOWN), plus 
+	// two cells that are not a doorway.
+	// These cells are white on the planning spreadsheet
+	@Test
+	public void FourDoorDirections() {
+		BoardCell room = board.getCellAt(8, 3);
+		assertTrue(room.isDoorway());
+		assertEquals(DoorDirection.RIGHT, room.getDoorDirection());
+		room = board.getCellAt(3, 6);
+		assertTrue(room.isDoorway());
+		assertEquals(DoorDirection.LEFT, room.getDoorDirection());
+		room = board.getCellAt(9, 12);
+		assertTrue(room.isDoorway());
+		assertEquals(DoorDirection.UP, room.getDoorDirection());
+		room = board.getCellAt(14, 1);
+		assertTrue(room.isDoorway());
+		assertEquals(DoorDirection.DOWN, room.getDoorDirection());
+		// Test that room pieces that aren't doors know it
+		room = board.getCellAt(17, 6);
+		assertFalse(room.isDoorway());	
+		// Test that walkways are not doors
+		BoardCell cell = board.getCellAt(0, 4);
+		assertFalse(cell.isDoorway());		
+
+	}
+	
+	// Test that we have the correct number of doors
+	@Test
+	public void testNumberOfDoorways() 
+	{
+		int numDoors = 0;
+		for (int row=0; row<board.getNumRows(); row++)
+			for (int col=0; col<board.getNumColumns(); col++) {
+				BoardCell cell = board.getCellAt(row, col);
+				if (cell.isDoorway())
+					numDoors++;
+			}
+		Assert.assertEquals(13, numDoors);
+	}
+
+	// Test a few room cells to ensure the room initial is correct.
+	@Test
+	public void testRoomInitials() {
+		// Test first cell in room
+		assertEquals('F', board.getCellAt(5, 0).getInitial());
+		assertEquals('G', board.getCellAt(12, 0).getInitial());
+		assertEquals('I', board.getCellAt(11, 16).getInitial());
+		// Test last cell in room
+		assertEquals('C', board.getCellAt(17, 9).getInitial());
+		assertEquals('B', board.getCellAt(6, 13).getInitial());
+		// Test a walkway
+		assertEquals('W', board.getCellAt(0, 5).getInitial());
+		// Test the closet
+		assertEquals('X', board.getCellAt(0,0).getInitial());
+	}
+	
+
+}
